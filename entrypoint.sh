5191780d7fac35ad8c59c9ee0a863f2faa3a51e7
Matthias Lackenbucher <matthias.lackenbucher@saltcreek.at>
2020-04-17 11:46:11 +0200

    added input `use-git-branch-as-tag`; updated license; updated README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,14 @@ DOCKER_TOKEN=$1
 DOCKER_IMAGE_NAME=$2
 DOCKER_IMAGE_TAG=$3
 EXTRACT_TAG_FROM_GIT_REF=$4
+USE_BRANCH_NAME_AS_TAG=$5
 
 if [ $EXTRACT_TAG_FROM_GIT_REF == "true" ]; then
   DOCKER_IMAGE_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
+fi
+
+if [ USE_BRANCH_NAME_AS_TAG == "true" ]; then
+  DOCKER_IMAGE_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
 fi
 
 DOCKER_IMAGE_FULL_PATH=$(echo docker.pkg.github.com/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} | tr '[:upper:]' '[:lower:]')
